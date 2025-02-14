fdab2f8eee5f8c05ec115c10863d40f289c00565
Kagayaku Kagami <jianyao2555@gmail.com>
2025-02-15 04:51:20 +0700

    added tooltip component

--- .vitepress/theme/components/VPDateTime.vue
+++ .vitepress/theme/components/VPDateTime.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref, computed, onMounted, watchEffect } from 'vue';
 import { useData } from 'vitepress/dist/client/theme-default/composables/data'
+import VPTooltip from './VPTooltip.vue';
 
 const { theme, lang } = useData()
 const props = defineProps({
@@ -26,7 +27,8 @@ const datetime = ref('');
 
 
 onMounted(() => {
-  watchEffect(() => {
+  watchEffect(() => 
+  {    
     datetime.value = new Intl.DateTimeFormat(
       theme.value.lastUpdated?.formatOptions?.forceLocale ? lang.value : undefined,
       theme.value.lastUpdated?.formatOptions ?? {
@@ -43,7 +45,7 @@ const formattedDate = computed(() => datetime.value);
   <p class="datetime-container" >
     <span class="label-text" v-if="label">{{ label }}: </span>
     <time :datetime="isoDatetime" class="date">{{ formattedDate }}</time>
-    <div class="tooltip" v-if="toolTip">{{ toolTip }}</div>
+    <VPTooltip v-if="toolTip">{{ toolTip }}</VPTooltip>
   </p>
 </template>
 
@@ -65,40 +67,5 @@ const formattedDate = computed(() => datetime.value);
 .datetime-container:hover .tooltip {
   visibility: visible;
   opacity: 1;
-}
-
-.tooltip {
-  opacity: 0;
-  transition: opacity 0.2s;
-  visibility: hidden;
-  
-  background-color: var(--vp-c-bg-soft);
-  border: 1px solid var(--vp-custom-block-info-border);
-  color: var(--vp-c-text-1);
-
-  box-shadow: var(--vp-shadow-3);
-  border-radius: 6px;
-  padding: 6px 16px;
-
-  text-align: center;
-  line-height: 24px;
-  white-space: nowrap;
-  
-  position: absolute;
-  margin-left: -50%; /* Use half of the width (120/2 = 60), to center the tooltip */
-  top: 100%;
-  left: 50%;
-  z-index: 1;
-}
-
-.tooltip::after {
-  content: " ";
-  position: absolute;
-  bottom: 100%;  /* At the top of the tooltip */
-  left: 50%;
-  margin-left: -5px;
-  border-width: 5px;
-  border-style: solid;
-  border-color: transparent transparent var(--vp-c-bg-soft) transparent;
 }
 </style>
---
+++ .vitepress/theme/components/VPTooltip.vue
@@ -0,0 +1,44 @@
+<script setup></script>
+
+<template>
+  <div class="tooltip">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.tooltip {
+  opacity: 0;
+  transition: opacity 0.2s;
+  visibility: hidden;
+  
+  background-color: var(--vp-c-bg-soft);
+  border: 1px solid var(--vp-custom-block-info-border);
+  color: var(--vp-c-text-1);
+
+  box-shadow: var(--vp-shadow-3);
+  border-radius: 6px;
+  padding: 6px 16px;
+
+  text-align: center;
+  line-height: 24px;
+  white-space: nowrap;
+  
+  position: absolute;
+  margin-left: -50%; /* Use half of the width (120/2 = 60), to center the tooltip */
+  top: 100%;
+  left: 50%;
+  z-index: 1;
+}
+
+.tooltip::after {
+  content: " ";
+  position: absolute;
+  bottom: 100%;  /* At the top of the tooltip */
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent transparent var(--vp-c-bg-soft) transparent;
+}
+</style>
